7651309b974e743ab6041fb3997c0ca497ced279
sairamreddy <sairamreddy2211@gmail.com>
2025-02-24 00:17:04 +0530

    day 10 timer css started

--- src/app/components/play/play.component.ts
+++ src/app/components/play/play.component.ts
@@ -8,11 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './play.component.scss'
 })
 export class PlayComponent {
-  drops:any = []
-
+  dateFormat='';
+  timeFormat='';
   ngOnInit(){ 
-    for (let i = 0; i < 50; i++) {
-      this.drops.push(1)
-    }
+    const date = new Date();
+
+
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// Format date manually
+this.dateFormat = `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+
+// F
+// Format time
+this.timeFormat = date.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+});
+
   }
 }
